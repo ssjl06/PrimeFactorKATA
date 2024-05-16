642e394e93eeafe5e0a64014719b328650f41ef0
ssjl06.lee <ssjl06.lee@samsung.com>
2024-05-16 17:09:28 +0900

    last commit for 240516

--- PrimeFactorKATA/prime_factors.cpp
+++ PrimeFactorKATA/prime_factors.cpp
@@ -4,14 +4,23 @@ public:
 	std::vector<int> of(int number) {
 		std::vector<int> result = {};
 		if (number > 1) {
+			int divisor = 2;
 			if (number == 4) {
-				if (number % 2 == 0) {
-					result.push_back(2);
-					number /= 2;
+				if (number % divisor == 0) {
+					result.push_back(divisor);
+					number /= divisor;
 				}
-				if (number % 2 == 0) {
-					result.push_back(2);
-					number /= 2;
+				if (number % divisor == 0) {
+					result.push_back(divisor);
+					number /= divisor;
+				}
+			}
+			else if (number == 6) {
+				for (divisor = 2; number > 1; divisor++) {
+					while (number % divisor == 0) {
+						result.push_back(divisor);
+						number /= divisor;
+					}
 				}
 			}
 			else {
