afda7cedfea0f76430ebd5d3142c368136652be6
ssjl06.lee <ssjl06.lee@samsung.com>
2024-05-16 16:56:11 +0900

    implementation that passes second test

--- PrimeFactorKATA/prime_factors.cpp
+++ PrimeFactorKATA/prime_factors.cpp
@@ -2,6 +2,9 @@
 class PrimeFactor {
 public:
 	std::vector<int> of(int i) {
+		if (i == 2) {
+			return { 2 };
+		}
 		return {};
 	}
 };
--- PrimeFactorTest/prime_factors_test.cpp
+++ PrimeFactorTest/prime_factors_test.cpp
@@ -2,8 +2,18 @@
 #include "../PrimeFactorKATA/prime_factors.cpp"
 #include <vector>
 
-TEST(PrimeFactors, Of1) {
+class PrimeFixture : public testing::Test {
+public:
 	PrimeFactor prime_factor;
-	std::vector<int> expected = {};
+	std::vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
